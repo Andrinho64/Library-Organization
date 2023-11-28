930a235e0cff8e227132bbf6fd30862af7ab663b
Andrew Caetano <andrewr_caetano@hotmail.com>
2023-11-28 11:45:06 -0300

    [Andrew Caetano] Library Organization

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 const { books } = require('../data/library');
 
 // Requisito 1
-const fantasyOrScienceFiction = () => {
-  // escreva seu código aqui
-};
-
+const fantasyOrScienceFiction = () => (
+  books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
+);
 // Requisito 2
 const oldBooksOrdered = (year) => {
-  // escreva seu código aqui
+  const maxYearsAgo = 60;
+  const cutoffYear = year - maxYearsAgo;
+
+  return books
+    .filter((book) => book.releaseYear <= cutoffYear)
+    .sort((a, b) => a.releaseYear - b.releaseYear);
 };
 
 // Requisito 3
-const booksByAuthorBirthYear = (birthYear) => {
-  // escreva seu código aqui
-};
+const booksByAuthorBirthYear = (birthYear) => books
+  .filter((book) => book.author.birthYear === birthYear)
+  .map((book) => book.name);
 
 // Requisito 4
-const fantasyOrScienceFictionAuthors = () => {
-  // escreva seu código aqui
-};
+const fantasyOrScienceFictionAuthors = () => books
+  .filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
+  .map((book) => book.author.name)
+  .sort();
 
 // Requisito 5
 const oldBooks = (year) => {
-  // escreva seu código aqui
+  const maxYearsAgo = 60;
+  const cutoffYear = year - maxYearsAgo;
+
+  return books
+    .filter((book) => book.releaseYear <= cutoffYear)
+    .map((book) => book.name);
 };
 
 // Requisito 6
 const authorWith3DotsOnName = () => {
-  // escreva seu código aqui
+  const bookWith3DotsAuthor = books.find((book) => /^([A-Z]\. ){2}[A-Z]\./.test(book.author.name));
+  return bookWith3DotsAuthor ? bookWith3DotsAuthor.name : null;
 };
 
 module.exports = {
